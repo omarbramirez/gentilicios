0306b66744193a2ec436eef3e195f4569811457a
omarbramirez <omar.ramirez94@hotmail.es>
2025-04-06 14:08:15 -0600

    correction on info segment

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import { useState } from "react";
 import ModernTable from "./components/ModernTable";
 import SearchBar from "./components/SearchBar";
 import ProyectoInfo from "./components/ProyectoInfo";
+import searchIcon from "./assets/icons/search.svg";
+import mapIcon from "./assets/icons/map.svg";
+import importantIcon from "./assets/icons/important.svg";
 
 function App() {
-  const [showInfo, setShowInfo] = useState(true);
+  const [showButtons, setShowButtons] = useState(true);
+  const [showInfo, setShowInfo] = useState(false);
   const [showSearchBar, setShowSearchBar] = useState(false);
   const [showModernTable, setShowModernTable] = useState(false);
 
@@ -12,12 +16,21 @@ function App() {
     setShowSearchBar(true);
     setShowModernTable(false);
     setShowInfo(false);
+    setShowButtons(false);
   };
 
   const toggleModernTable = () => {
     setShowModernTable(true);
     setShowSearchBar(false);
     setShowInfo(false);
+    setShowButtons(false);
+  };
+
+  const toggleInfo = () => {
+    setShowInfo(true);
+    setShowSearchBar(false);
+    setShowModernTable(false);
+    setShowButtons(false);
   };
 
   return (
@@ -27,15 +40,42 @@ function App() {
         width: "100%",
       }}
     >
-      {showInfo && (
-        <ProyectoInfo
-          toggleSearchBar={toggleSearchBar}
-          toggleModernTable={toggleModernTable}
-        />
+      <h1 className="header">Glosario de Gentilicios del Ecuador</h1>
+      {showButtons && (
+        <div className="proyecto-info-module">
+          <button onClick={toggleInfo} className="icon-button">
+            <img src={importantIcon} alt="justificación" />
+            <b>Sobre este proyecto</b>
+            <span className="icon-desc">
+              Conoce el trabajo de la Comisión de Lexicografía detrás de esta
+              herramienta y la importancia de preservar el patrimonio
+              lingüístico ecuatoriano.
+            </span>
+          </button>
+          <button onClick={toggleSearchBar} className="icon-button">
+            <img src={searchIcon} alt="busqueda" />
+            <b>Buscador de gentilicios</b>
+            <span className="icon-desc">
+              Consulta de forma rápida y sencilla los gentilicios de provincias,
+              capitales y cantones del Ecuador.
+            </span>
+          </button>
+          <button onClick={toggleSearchBar} className="icon-button">
+            <img src={mapIcon} alt="regiones" />
+            <b>Tabla general de gentilicios</b>
+            <span className="icon-desc">
+              Consulta de forma rápida y sencilla los gentilicios de provincias,
+              capitales y cantones del Ecuador.
+            </span>
+          </button>
+        </div>
       )}
-      {!showInfo && (
+      {!showButtons && (
         <>
           <div className="toggle-buttons">
+            <button onClick={toggleInfo} disabled={showInfo} id={"infoButton"}>
+              {showInfo ? "Información Activa" : "Mostrar más Información"}
+            </button>
             <button
               onClick={toggleSearchBar}
               disabled={showSearchBar}
@@ -57,6 +97,7 @@ function App() {
             </button>
           </div>
           <div id="modules">
+            {showInfo && <ProyectoInfo />}
             {showSearchBar && <SearchBar />}
             {showModernTable && <ModernTable />}
           </div>
